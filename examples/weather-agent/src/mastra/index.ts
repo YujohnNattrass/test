b6c8c90a7dfb902f54daf1a56e1c99b636d8dbde
YujohnNattrass <46704329+YujohnNattrass@users.noreply.github.com>
2025-04-16 07:37:10 -0700

    Update index.ts

--- examples/weather-agent/src/mastra/index.ts
+++ examples/weather-agent/src/mastra/index.ts
@@ -3,7 +3,7 @@ import { Mastra } from '@mastra/core';
 import { weatherAgent } from './agents';
 import { weatherWorkflow } from './workflows';
 
-const mastra = new Mastra({
+export const mastra = new Mastra({
   agents: { weatherAgent },
   workflows: { weatherWorkflow },
 });
